bf6076afca3ba4662b73efafd2d630324aa5b84e
qoqomi <energyy044@gmail.com>
2026-02-11 17:06:16 +0900

    fix: info내 graphql추가

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,11 +1,34 @@
 import Text from 'components/Text'
+import { graphql } from 'gatsby';
+import React, { FunctionComponent } from "react";
 
-const InfoPage = () => {
-    return (
-        <div>
-            <Text text='Hello'/>
-        </div>
-    )
-}
+type InfoPageProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        author: string;
+        description: string;
+        title: string;
+      };
+    };
+  };
+};
+
+const InfoPage: FunctionComponent<InfoPageProps> = function({data}:InfoPageProps) {
+  return <Text text={`Hello ${data.site.siteMetadata.title}`} />;
+};      
 
 export default InfoPage;
+
+
+export const metaDataQuery = graphql`
+query {
+  site {
+    siteMetadata {
+      author
+      description
+      title
+    }
+  }
+}
+`
